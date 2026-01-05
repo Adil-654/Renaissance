8793f48482afdb2487f5a30b0fad4dee0c27f695
sanjana-panchal27 <sanjanapanchal2713@gmail.com>
2026-01-05 21:06:22 +0530

    server

--- index.js
+++ index.js
@@ -5,7 +5,24 @@
     //npm i express
 //packge.json  package.logjson
     //npm init 
+    //npm init -y
 
 
-    console.log("This is my first project")
-    console.log("Hi")
+    // console.log("This is my first project")
+    // console.log("Hi")
+
+
+
+const express=require('express')
+const app=express()
+const PORT=4000
+
+// app.get('/',(req,res)=>{
+//     res.send("Hello")
+// }
+// )
+app.listen(PORT,()=>{
+    console.log("Port is running")
+})
+
+
